e2978d234af6043a710e8765196c8fcf87511854
hnsyssz@gmail.com <hnsyssz@gmail.com>
2017-08-31 10:32:17 +0800

    init

---
+++ Watch/Project/Objects/main.d
@@ -0,0 +1,9 @@
+.\objects\main.o: ..\User\src\main.c
+.\objects\main.o: ..\User\inc\includes.h
+.\objects\main.o: ..\CMSIS\stm32f4xx.h
+.\objects\main.o: ..\CMSIS\core_cm4.h
+.\objects\main.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: ..\CMSIS\core_cmInstr.h
+.\objects\main.o: ..\CMSIS\core_cmFunc.h
+.\objects\main.o: ..\CMSIS\core_cm4_simd.h
+.\objects\main.o: ..\CMSIS\system_stm32f4xx.h
---
+++ Watch/User/inc/includes.h
@@ -0,0 +1,33 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+//存放用户自定义的.h文件
+
+#include "stm32f4xx.h"
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
